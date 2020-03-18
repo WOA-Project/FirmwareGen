3c23350d970c603803710003d27f10f4009df2ef
Gustave Monce <gustave.monce@outlook.com>
2020-03-18 17:34:56 +0100

    Update firmware gen

--- DeviceProfiles/CitymanProfile.cs
+++ DeviceProfiles/CitymanProfile.cs
@@ -9,13 +9,20 @@
 
         public string DriverCommand(string DriverFolder)
         {
-            return $"/Driver:\"{DriverFolder}\\msm8992-8994\" " +
-                $"/Driver:\"{DriverFolder}\\msm8994\" " +
-                $"/Driver:\"{DriverFolder}\\support-desktop\" " +
-                $"/Driver:\"{DriverFolder}\\support-aarch64\" " +
-                $"/Driver:\"{DriverFolder}\\configurations\\oems\\specifics-mmo\" " +
-                $"/Driver:\"{DriverFolder}\\configurations\\soc-final\" " +
-                $"/Driver:\"{DriverFolder}\\configurations\\devices\\specifics-cityman\" /Recurse";
+            return $"/Driver:\"{DriverFolder}\\DEVICE.INPUT.SYNAPTICS_RMI4\" " +
+                    $"/Driver:\"{DriverFolder}\\DEVICE.SOC_QC8994.CITYMAN\" " +
+                    $"/Driver:\"{DriverFolder}\\DEVICE.USB.MMO_USBC\" " +
+                    $"/Driver:\"{DriverFolder}\\OEM.SOC_QC8994.MMO\" " +
+                    $"/Driver:\"{DriverFolder}\\OEM.SOC_QC8994.MMO_SOC8994\" " +
+                    $"/Driver:\"{DriverFolder}\\PLATFORM.SOC_QC8994.BASE\" " +
+                    $"/Driver:\"{DriverFolder}\\PLATFORM.SOC_QC8994.MMO\" " +
+                    $"/Driver:\"{DriverFolder}\\PLATFORM.SOC_QC8994.SOC8994\" " +
+                    $"/Driver:\"{DriverFolder}\\PLATFORM.SOC_QC8994.SOC8994AB\" " +
+                    $"/Driver:\"{DriverFolder}\\SUPPORT.DESKTOP.BASE\" " +
+                    $"/Driver:\"{DriverFolder}\\SUPPORT.DESKTOP.EXTRAS\" " +
+                    $"/Driver:\"{DriverFolder}\\SUPPORT.DESKTOP.MMO_EXTRAS\" " +
+                    $"/Driver:\"{DriverFolder}\\SUPPORT.DESKTOP.MOBILE_BRIDGE\" " +
+                    $"/Driver:\"{DriverFolder}\\SUPPORT.DESKTOP.MOBILE_COMPONENTS\" /Recurse";
         }
 
         public string FFUFileName(string OSVersion, string Language, string Sku)
