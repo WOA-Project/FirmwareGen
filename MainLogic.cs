c4df69fcf43e9da2e427fe6270902565d5e32efd
Gustave Monce <gustave.monce@outlook.com>
2021-08-18 19:08:24 +0200

    Move away some of the code in MainLogic to an utility class

--- MainLogic.cs
+++ MainLogic.cs
@@ -1,8 +1,6 @@
 ﻿using FirmwareGen.DeviceProfiles;
-using FirmwareGen.Extensions;
 using FirmwareGen.Streams;
 using System;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using static FirmwareGen.CLIOptions;
@@ -44,104 +42,23 @@ namespace FirmwareGen
             return true;
         }
 
-        public static string MountVHD(string VHDPath, bool readOnly)
-        {
-            Logging.Log("Mounting " + VHDPath + (readOnly ? " as read only" : "") + "...");
-            string id = VHDUtils.MountVHD(VHDPath, readOnly);
-            Logging.Log(id, Logging.LoggingLevel.Warning);
-            return id;
-        }
-
-        public static void DismountVHD(string VHDPath)
-        {
-            Logging.Log("Dismounting " + VHDPath + "...");
-            VHDUtils.UnmountVHD(VHDPath);
-        }
-
-        public static string GetVHDLetter(string DiskId)
-        {
-            string TVHDLetter = null;
-            Process proc = new();
-            proc.StartInfo = new ProcessStartInfo("powershell.exe", "-command \"(Get-Partition -DiskNumber " + DiskId + " | Where {$_.DriveLetter}).DriveLetter\"")
-            {
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                CreateNoWindow = true
-            };
-            proc.Start();
-            while (!proc.StandardOutput.EndOfStream)
-            {
-                string line = proc.StandardOutput.ReadLine();
-                Logging.Log("VHD mounted at: " + line);
-                TVHDLetter = line + ":";
-            }
-            proc.WaitForExit();
-            return TVHDLetter;
-        }
-
-        public static void RunProgram(string Program, string Arguments)
-        {
-            Process proc = new();
-            proc.StartInfo = new ProcessStartInfo(Program, Arguments)
-            {
-                WindowStyle = ProcessWindowStyle.Hidden,
-                UseShellExecute = false
-            };
-            proc.Start();
-            proc.WaitForExit();
-        }
-
-        public static void CopyFile(string Source, string Dest)
-        {
-            FileInfo _source = new(Source);
-            FileInfo _destination = new(Dest);
-
-            DateTime startTime = DateTime.Now;
-            _source.CopyTo(_destination, x => Logging.ShowProgress(100, startTime, (UInt64)x, (UInt64)x, false));
-            Logging.Log("");
-        }
-
         public static void GenerateWindowsBaseVHDX(GenerateWindowsOptions options)
         {
             const string BlankVHD = "blank.vhdx";
             const string wimlib = "wimlib-imagex.exe";
-
             const string SystemPartition = "Y:";
 
             Logging.Log("Copying Blank Main VHD");
-            CopyFile(BlankVHD, options.Output);
-
-            string DiskId = MountVHD(options.Output, false);
-
-            string VHDLetter = GetVHDLetter(DiskId);
-
-            Logging.Log("Applying image");
-            RunProgram(wimlib, $"apply {options.WindowsDVD}\\sources\\install.wim {options.WindowsIndex} {VHDLetter} --compact=LZX");
-
-            Logging.Log("Slab optimization");
-            RunProgram("defrag.exe", $"{VHDLetter} /K /X");
-
-            Logging.Log("Applying compact flags");
-            RunProgram("reg.exe", $"load HKLM\\RTSYSTEM {VHDLetter}\\Windows\\System32\\config\\SYSTEM");
-            RunProgram("reg.exe", @"add HKLM\RTSYSTEM\Setup /v Compact /t REG_DWORD /d 1");
-            RunProgram("reg.exe", @"unload HKLM\RTSYSTEM");
-            RunProgram("reg.exe", $"load HKLM\\RTSOFTWARE {VHDLetter}\\Windows\\System32\\config\\SOFTWARE");
-            RunProgram("reg.exe", "add \"HKLM\\RTSOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Wof\" /v ForceAlgorithm /t REG_DWORD /d 1");
-            RunProgram("reg.exe", @"unload HKLM\RTSOFTWARE");
-
-            Logging.Log("Mounting SYSTEM");
-            RunProgram("powershell.exe", "-command \"Get-Partition -DiskNumber " + DiskId + " | Where {$_.Type -eq 'System'} | Set-Partition -NewDriveLetter '" + SystemPartition + "'.Substring(0,1)\"");
-
-            Logging.Log("Configuring boot");
-            RunProgram("bcdboot.exe", $"{VHDLetter}\\Windows /s {SystemPartition} /f UEFI /l en-us");
-
-            RunProgram("bcdedit.exe", $"/store {SystemPartition}\\EFI\\Microsoft\\Boot\\BCD " + "/set {default} testsigning on");
-            RunProgram("bcdedit.exe", $"/store {SystemPartition}\\EFI\\Microsoft\\Boot\\BCD " + "/set {default} nointegritychecks on");
-
-            Logging.Log("Unmounting SYSTEM");
-            RunProgram("powershell.exe", "-command \"Get-Partition -DiskNumber " + DiskId + " | Where {$_.Type -eq 'System'} | Remove-PartitionAccessPath -accesspath '" + SystemPartition + "'\"");
-
-            DismountVHD(options.Output);
+            VolumeUtils.CopyFile(BlankVHD, options.Output);
+            string DiskId = VolumeUtils.MountVirtualHardDisk(options.Output, false);
+            string VHDLetter = VolumeUtils.GetVirtualHardDiskLetterFromDiskID(DiskId);
+            VolumeUtils.ApplyWindowsImageFromDVD(wimlib, options.WindowsDVD, options.WindowsIndex, VHDLetter);
+            VolumeUtils.PerformSlabOptimization(VHDLetter);
+            VolumeUtils.ApplyCompactFlagsToImage(VHDLetter);
+            VolumeUtils.MountSystemPartition(DiskId, SystemPartition);
+            VolumeUtils.ConfigureBootManager(VHDLetter, SystemPartition);
+            VolumeUtils.UnmountSystemPartition(DiskId, SystemPartition);
+            VolumeUtils.DismountVirtualHardDisk(options.Output);
         }
 
         public static void GenerateWindowsFFU(GenerateWindowsFFUOptions options)
@@ -159,42 +76,36 @@ namespace FirmwareGen
             foreach (IDeviceProfile deviceProfile in deviceProfiles)
             {
                 const string TmpVHD = tmp + @"\temp.vhdx";
+
                 Logging.Log("Copying Main VHD");
-                CopyFile(options.Input, TmpVHD);
-
-                string DiskId = MountVHD(TmpVHD, false);
-
-                string TVHDLetter = GetVHDLetter(DiskId);
-
+                VolumeUtils.CopyFile(options.Input, TmpVHD);
+                string DiskId = VolumeUtils.MountVirtualHardDisk(TmpVHD, false);
+                string TVHDLetter = VolumeUtils.GetVirtualHardDiskLetterFromDiskID(DiskId);
                 Logging.Log("Writing Bootloader");
                 WriteBootLoaderToDisk(DiskId, deviceProfile);
-
-                DismountVHD(TmpVHD);
-
-                DiskId = MountVHD(TmpVHD, false);
+                VolumeUtils.DismountVirtualHardDisk(TmpVHD);
+                DiskId = VolumeUtils.MountVirtualHardDisk(TmpVHD, false);
 
                 Logging.Log("Writing UEFI");
                 File.Copy(deviceProfile.UEFIELFPath(), $"{TVHDLetter}\\EFIESP\\UEFI.elf", true);
 
                 if (deviceProfile.SupplementaryBCDCommands().Length > 0)
                 {
-                    Logging.Log("Mounting SYSTEM");
-                    RunProgram("powershell.exe", "-command \"Get-Partition -DiskNumber " + DiskId + " | Where {$_.Type -eq 'System'} | Set-Partition -NewDriveLetter '" + SystemPartition + "'.Substring(0,1)\"");
+                    VolumeUtils.MountSystemPartition(DiskId, SystemPartition);
 
                     Logging.Log("Configuring supplemental boot");
                     foreach (string command in deviceProfile.SupplementaryBCDCommands())
                     {
-                        RunProgram("bcdedit.exe", $"/store {SystemPartition}\\EFI\\Microsoft\\Boot\\BCD " + command);
+                        VolumeUtils.RunProgram("bcdedit.exe", $"/store {SystemPartition}\\EFI\\Microsoft\\Boot\\BCD " + command);
                     }
 
-                    Logging.Log("Unmounting SYSTEM");
-                    RunProgram("powershell.exe", "-command \"Get-Partition -DiskNumber " + DiskId + " | Where {$_.Type -eq 'System'} | Remove-PartitionAccessPath -accesspath '" + SystemPartition + "'\"");
+                    VolumeUtils.UnmountSystemPartition(DiskId, SystemPartition);
                 }
 
                 Logging.Log("Adding drivers");
-                RunProgram(DriverUpdater, $"{deviceProfile.DriverCommand(options.DriverPack)} {options.DriverPack} {TVHDLetter}");
+                VolumeUtils.RunProgram(DriverUpdater, $"{deviceProfile.DriverCommand(options.DriverPack)} {options.DriverPack} {TVHDLetter}");
 
-                DismountVHD(TmpVHD);
+                VolumeUtils.DismountVirtualHardDisk(TmpVHD);
 
                 Logging.Log("Making FFU");
                 string version = options.WindowsVer;
@@ -203,7 +114,7 @@ namespace FirmwareGen
                     version = string.Join(".", version.Split(".").Skip(2));
                 }
 
-                RunProgram(Img2Ffu, $"-i {TmpVHD} -f \"{options.Output + "\\" + deviceProfile.FFUFileName(version, "en-us", "PROFESSIONAL")}\" -p {deviceProfile.PlatformID()} -o {options.WindowsVer}");
+                VolumeUtils.RunProgram(Img2Ffu, $"-i {TmpVHD} -f \"{options.Output + "\\" + deviceProfile.FFUFileName(version, "en-us", "PROFESSIONAL")}\" -p {deviceProfile.PlatformID()} -o {options.WindowsVer}");
 
                 Logging.Log("Deleting Temp VHD");
                 File.Delete(TmpVHD);
@@ -249,23 +160,16 @@ namespace FirmwareGen
             {
                 const string TmpVHD = tmp + @"\temp.vhdx";
                 Logging.Log("Copying Main VHD");
-                CopyFile(options.Input, TmpVHD);
-
-                string DiskId = MountVHD(TmpVHD, false);
-                _ = GetVHDLetter(DiskId);
-
+                VolumeUtils.CopyFile(options.Input, TmpVHD);
+                string DiskId = VolumeUtils.MountVirtualHardDisk(TmpVHD, false);
+                _ = VolumeUtils.GetVirtualHardDiskLetterFromDiskID(DiskId);
                 Logging.Log("Writing Bootloader");
                 WriteBootLoaderToDisk(DiskId, deviceProfile);
-
-                DismountVHD(TmpVHD);
-
-                DiskId = MountVHD(TmpVHD, true);
-
+                VolumeUtils.DismountVirtualHardDisk(TmpVHD);
+                DiskId = VolumeUtils.MountVirtualHardDisk(TmpVHD, false);
                 Logging.Log("Making FFU");
-                RunProgram(Img2Ffu, $"-i \\\\.\\PhysicalDrive{DiskId} -f \"{options.Output + "\\" + deviceProfile.FFUFileName(options.Ver, "en-us", "PROFESSIONAL")}\" -p {deviceProfile.PlatformID()} -o {options.Ver}");
-
-                DismountVHD(TmpVHD);
-
+                VolumeUtils.RunProgram(Img2Ffu, $"-i \\\\.\\PhysicalDrive{DiskId} -f \"{options.Output + "\\" + deviceProfile.FFUFileName(options.Ver, "en-us", "PROFESSIONAL")}\" -p {deviceProfile.PlatformID()} -o {options.Ver}");
+                VolumeUtils.DismountVirtualHardDisk(TmpVHD);
                 Logging.Log("Deleting Temp VHD");
                 File.Delete(TmpVHD);
             }
---
+++ VolumeUtils.cs
@@ -0,0 +1,112 @@
+﻿using FirmwareGen.Extensions;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace FirmwareGen
+{
+    public static class VolumeUtils
+    {
+        public static void UnmountSystemPartition(string DiskId, string SystemPartition)
+        {
+            Logging.Log("Unmounting SYSTEM");
+            RunProgram("powershell.exe", "-command \"Get-Partition -DiskNumber " + DiskId + " | Where {$_.Type -eq 'System'} | Remove-PartitionAccessPath -accesspath '" + SystemPartition + "'\"");
+        }
+
+        public static void ConfigureBootManager(string VHDLetter, string SystemPartition)
+        {
+            Logging.Log("Configuring boot");
+            RunProgram("bcdboot.exe", $"{VHDLetter}\\Windows /s {SystemPartition} /f UEFI /l en-us");
+
+            RunProgram("bcdedit.exe", $"/store {SystemPartition}\\EFI\\Microsoft\\Boot\\BCD " + "/set {default} testsigning on");
+            RunProgram("bcdedit.exe", $"/store {SystemPartition}\\EFI\\Microsoft\\Boot\\BCD " + "/set {default} nointegritychecks on");
+            RunProgram("bcdedit.exe", $"/store {SystemPartition}\\EFI\\Microsoft\\Boot\\BCD " + "/dbgsettings USB TARGETNAME:WOATARGET");
+        }
+
+        public static void MountSystemPartition(string DiskId, string SystemPartition)
+        {
+            Logging.Log("Mounting SYSTEM");
+            RunProgram("powershell.exe", "-command \"Get-Partition -DiskNumber " + DiskId + " | Where {$_.Type -eq 'System'} | Set-Partition -NewDriveLetter '" + SystemPartition + "'.Substring(0,1)\"");
+        }
+
+        public static void ApplyCompactFlagsToImage(string VHDLetter)
+        {
+            Logging.Log("Applying compact flags");
+            RunProgram("reg.exe", $"load HKLM\\RTSYSTEM {VHDLetter}\\Windows\\System32\\config\\SYSTEM");
+            RunProgram("reg.exe", @"add HKLM\RTSYSTEM\Setup /v Compact /t REG_DWORD /d 1");
+            RunProgram("reg.exe", @"unload HKLM\RTSYSTEM");
+            RunProgram("reg.exe", $"load HKLM\\RTSOFTWARE {VHDLetter}\\Windows\\System32\\config\\SOFTWARE");
+            RunProgram("reg.exe", "add \"HKLM\\RTSOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Wof\" /v ForceAlgorithm /t REG_DWORD /d 1");
+            RunProgram("reg.exe", @"unload HKLM\RTSOFTWARE");
+        }
+
+        public static void PerformSlabOptimization(string VHDLetter)
+        {
+            Logging.Log("Slab optimization");
+            RunProgram("defrag.exe", $"{VHDLetter} /K /X");
+        }
+
+        public static void RunProgram(string Program, string Arguments)
+        {
+            Process proc = new();
+            proc.StartInfo = new ProcessStartInfo(Program, Arguments)
+            {
+                WindowStyle = ProcessWindowStyle.Hidden,
+                UseShellExecute = false
+            };
+            proc.Start();
+            proc.WaitForExit();
+        }
+
+        public static void ApplyWindowsImageFromDVD(string wimlib, string WindowsDVD, string WindowsIndex, string VHDLetter)
+        {
+            Logging.Log("Applying image");
+            RunProgram(wimlib, $"apply {WindowsDVD}\\sources\\install.wim {WindowsIndex} {VHDLetter} --compact=LZX");
+        }
+
+        public static void CopyFile(string Source, string Dest)
+        {
+            FileInfo _source = new(Source);
+            FileInfo _destination = new(Dest);
+
+            DateTime startTime = DateTime.Now;
+            _source.CopyTo(_destination, x => Logging.ShowProgress(100, startTime, (UInt64)x, (UInt64)x, false));
+            Logging.Log("");
+        }
+
+        public static string MountVirtualHardDisk(string VHDPath, bool readOnly)
+        {
+            Logging.Log("Mounting " + VHDPath + (readOnly ? " as read only" : "") + "...");
+            string id = VHDUtils.MountVHD(VHDPath, readOnly);
+            Logging.Log(id, Logging.LoggingLevel.Warning);
+            return id;
+        }
+
+        public static void DismountVirtualHardDisk(string VHDPath)
+        {
+            Logging.Log("Dismounting " + VHDPath + "...");
+            VHDUtils.UnmountVHD(VHDPath);
+        }
+
+        public static string GetVirtualHardDiskLetterFromDiskID(string DiskId)
+        {
+            string TVHDLetter = null;
+            Process proc = new();
+            proc.StartInfo = new ProcessStartInfo("powershell.exe", "-command \"(Get-Partition -DiskNumber " + DiskId + " | Where {$_.DriveLetter}).DriveLetter\"")
+            {
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            };
+            proc.Start();
+            while (!proc.StandardOutput.EndOfStream)
+            {
+                string line = proc.StandardOutput.ReadLine();
+                Logging.Log("VHD mounted at: " + line);
+                TVHDLetter = line + ":";
+            }
+            proc.WaitForExit();
+            return TVHDLetter;
+        }
+    }
+}
