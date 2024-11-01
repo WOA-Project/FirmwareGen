11286ce08a74679daf4cb5fd95dbbcbf4ccfd384
Gustave Monce <gustave.monce@outlook.com>
2024-11-01 16:32:52 +0100

    Add an argument to sign ffu files during the build process

--- FirmwareGen/CommandLine/GenerateWindowsFFUOptions.cs
+++ FirmwareGen/CommandLine/GenerateWindowsFFUOptions.cs
@@ -40,5 +40,11 @@ namespace FirmwareGen.CommandLine
         {
             get; set;
         }
+
+        [Option('b', "secure-boot-signing-command", HelpText = "Optional command to sign the resulting FFU file to work in Secure Boot scenarios. The File to sign is passed as a path appended after the provided command string, and is the sole and only argument provided.", Default = "", Required = false)]
+        public required string SecureBootSigningCommand
+        {
+            get; set;
+        }
     }
 }
